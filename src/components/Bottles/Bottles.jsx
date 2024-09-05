5d0a13ed4bb5012e605d61342aa44e8a2c7dd303
Rafatl35 <rafatrahmano4@gmail.com>
2024-09-06 01:38:37 +0600

    Display Saved cart from Local storage

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Bottle from "../Bottle/Bottle";
 import './bottles.css';
 import { addToLs, getStoredCart } from "../../Utilities/localStorage";
+import Cart from "../Cart/Cart";
 
 const Bottles = () => {
     const [bottles, setBottles] = useState([]);
@@ -19,7 +20,18 @@ const Bottles = () => {
         console.log('colled the useEffect:',bottles.length)
        if(bottles.length > 0){
         const storedCart = getStoredCart();
-        console.log(storedCart);
+        console.log(storedCart, bottles);
+        const saveCart = [];
+
+        for(const id of storedCart){
+            console.log(id);
+            const bottle = bottles.find(bottle =>bottle.id===id);
+            if(bottle){
+                saveCart.push(bottle)
+            }
+        }
+        console.log('savedCart:',saveCart)
+        setCart(saveCart)
        }
     },[bottles])
 
@@ -32,7 +44,7 @@ const Bottles = () => {
     return (
         <div>
             <h2>Bottles Available: {bottles.length}</h2>
-            <h4>Cart:{cart.length}</h4>
+            <Cart cart={cart}></Cart>
             <div className="bottle-container">
             {
               bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle} handleAddToCart={handleAddToCart}></Bottle>)
